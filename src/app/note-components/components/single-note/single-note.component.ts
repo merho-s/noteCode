377a31880cc41f5b8d57fe7c3ca0b34a674f2f90
Mosko54 <moussaid08@outlook.fr>
2023-09-10 23:38:48 +0200

    fix not loading note on click on navigator back button

--- src/app/note-components/components/single-note/single-note.component.ts
+++ src/app/note-components/components/single-note/single-note.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input, OnChanges, AfterViewInit, AfterViewChecked } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Note } from 'src/app/core/models/note.interface';
 import { NoteService } from 'src/app/core/services/note.service';
-import { Observable, catchError, tap } from 'rxjs';
+import { Observable, Subscription, catchError, tap } from 'rxjs';
 import { HighlightService } from 'src/app/core/services/highlight.service';
 import { ControlValueAccessor } from '@angular/forms';
 
@@ -18,11 +18,24 @@ export class SingleNoteComponent {
   tagsEditingMode: boolean = false;
   note$!: Observable<Note>;
   codesEditingModes: boolean[] = [];
+  navigationEndSubscription$!: Subscription;
 
   constructor(private noteService: NoteService,
-              private route: ActivatedRoute) {}
+              private route: ActivatedRoute,
+              private router: Router) {}
 
   ngOnInit() {
+    this.navigationEndSubscription$ = this.router.events.pipe(
+      tap(event => {
+        if(event instanceof NavigationEnd) {
+          this.getNote();
+        }
+      })
+    ).subscribe();
+    this.getNote();
+  }
+
+  getNote() {
     const noteId = this.route.snapshot.params['id'];
     this.note$ = this.noteService.getNoteById(noteId).pipe(
       tap(note => {
@@ -31,6 +44,10 @@ export class SingleNoteComponent {
         }
       })
     );
+  }
+
+  ngOnDestroy() {
+    this.navigationEndSubscription$.unsubscribe();
   }
   
 }
